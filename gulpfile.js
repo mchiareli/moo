d31136a03f734eeeb3ed119561543ae386587a52
mchiareli <mchiareli@gmail.com>
2016-01-30 17:12:04 -0200

    adding grid :bowtie:

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@
 
 var gulp = require('gulp'),
     connect = require('gulp-connect'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    deploy = require('gulp-gh-pages');
 
 gulp.task('run',['build','server']);
 
@@ -20,7 +21,8 @@ gulp.task('server',['watch'],function() {
 
 gulp.task('watch',function(){
   gulp.watch(['src/scss/**/*.scss'],['build-css']);
-  gulp.watch(['vendor/**/*.html'],['build-vendor']);
+  gulp.watch(['vendor/**/*.html'],['build-vendor-html']);
+  gulp.watch(['vendor/scss/*.scss'],['build-vendor-css']);
 });
 
 gulp.task('default',['build']);
@@ -34,11 +36,20 @@ gulp.task('build-css',function(){
     .pipe(connect.reload());
 });
 
-gulp.task('build-vendor',function() {
+gulp.task('build-vendor',['build-vendor-css','build-vendor-html']);
+
+gulp.task('publish-vendor',['build'],function(){
+  return gulp.src("./dist/**/*")
+    .pipe(deploy());
+});
+
+gulp.task('build-vendor-html',function() {
   gulp.src('vendor/**/*.html')
       .pipe(gulp.dest('dist'))
       .pipe(connect.reload());
+});
 
+gulp.task('build-vendor-css',function() {
   gulp.src('vendor/scss/*.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('dist/css'))
